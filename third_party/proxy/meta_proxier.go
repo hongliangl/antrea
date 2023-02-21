b4541195a3bcd0c00aa3383ffe6fae99e7738de0
Hongliang Liu <lhongliang@vmware.com>
2023-02-21 12:57:55 +0800

    a
    Signed-off-by: Hongliang Liu <lhongliang@vmware.com>

--- third_party/proxy/meta_proxier.go
+++ third_party/proxy/meta_proxier.go
@@ -1,19 +1,17 @@
-/*
-Copyright 2019 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-/*
+// Copyright 2019 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 // Copyright 2020 Antrea Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,12 +26,11 @@ limitations under the License.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-Modifies:
-- Cleanup unused imports due to code removal and relocation
-- Remove NodeHandler member from metaProxier struct
-- Remove Sync() func as Provider interface removes it
-- Remove EndPointSlice handling
-*/
+// Modifies:
+// - Cleanup unused imports due to code removal and relocation
+// - Remove NodeHandler member from metaProxier struct
+// - Remove Sync() func as Provider interface removes it
+// - Remove EndPointSlice handling
 
 package proxy
 
--- third_party/proxy/types.go
+++ third_party/proxy/types.go
@@ -1,19 +1,17 @@
-/*
-Copyright 2015 The Kubernetes Authors.
+// Copyright 2015 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-/*
 // Copyright 2020 Antrea Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,12 +26,11 @@ limitations under the License.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-Modifies:
-- Replace import "k8s.io/kubernetes/pkg/proxy/config" with "antrea.io/antrea/third_party/proxy/config"
-- Remove config.EndpointSliceHandler, config.NodeHandler from Provider interface type
-- Remove NodeHandler, EndpointSliceHandler, Sync() from Provider interface
-- Add Run() to Provider interface
-*/
+// Modifies:
+// - Replace import "k8s.io/kubernetes/pkg/proxy/config" with "antrea.io/antrea/third_party/proxy/config"
+// - Remove config.EndpointSliceHandler, config.NodeHandler from Provider interface type
+// - Remove NodeHandler, EndpointSliceHandler, Sync() from Provider interface
+// - Add Run() to Provider interface
 
 package proxy
 
