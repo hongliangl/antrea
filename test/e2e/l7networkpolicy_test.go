8e261a73e4278571722ae09ab29d2ee39bed943e
Hongliang Liu <75655411+hongliangl@users.noreply.github.com>
2023-07-26 08:42:10 +0800

    Update l7networkpolicy_test.go

--- test/e2e/l7networkpolicy_test.go
+++ test/e2e/l7networkpolicy_test.go
@@ -319,7 +319,7 @@ func testL7NetworkPolicyTLS(t *testing.T, data *TestData) {
 	createL7NetworkPolicy(t, data, false, policyAllowSNIFacebook, 2, nil, clientPodLabels, ProtocolTCP, 443, l7ProtocolAllowsFacebook)
 	time.Sleep(networkPolicyDelay)
 
-	probeL7NetworkPolicyTLS(t, data, clientPodName, "apis.google.com", true)
+	probeL7NetworkPolicyTLS(t, data, clientPodName, "developers.google.com", true)
 	probeL7NetworkPolicyTLS(t, data, clientPodName, "www.facebook.com", false)
 
 	// Delete the first L7 NetworkPolicy that allows server name 'github.com'.
