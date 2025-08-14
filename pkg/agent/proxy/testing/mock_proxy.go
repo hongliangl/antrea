5d7faa9ccb6dc093e21dead9a986cd0e0b7581e6
Hongliang Liu <hongliang.liu@broadcom.com>
2025-08-14 16:45:42 +0800

    Refine AntreaProxy framework to eliminate duplicated processing in dual-stack
    This commit introduces a wrapper `ProxyServer`, which holds shared objects used by IPv4
    and IPv6 proxier. Common processing, such as running `serviceConfig`, `endpointSliceConfig`,
    `nodeConfig` and feature gates, are moved to `ProxyServer` to avoid duplicated processing
    when AntreaProxy is running in dual-stack.
    
    Additionally, this commit fixes the Prometheus metrics defined for AntreaProxy.
    
    This commit is also the foundation of introducing a single-instance `ProxyHealthServer` later,
    which should remain unique even in dual-stack.
    
    Signed-off-by: Hongliang Liu <hongliang.liu@broadcom.com>

--- pkg/agent/proxy/testing/mock_proxy.go
+++ pkg/agent/proxy/testing/mock_proxy.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Antrea Authors
+// Copyright 2025 Antrea Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -30,6 +30,8 @@ import (
 	openflow "antrea.io/antrea/pkg/ovs/openflow"
 	proxy "antrea.io/antrea/third_party/proxy"
 	gomock "go.uber.org/mock/gomock"
+	v1 "k8s.io/api/core/v1"
+	v10 "k8s.io/api/discovery/v1"
 )
 
 // MockProxier is a mock of Proxier interface.
@@ -54,20 +56,6 @@ func NewMockProxier(ctrl *gomock.Controller) *MockProxier {
 // EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockProxier) EXPECT() *MockProxierMockRecorder {
 	return m.recorder
-}
-
-// GetProxyProvider mocks base method.
-func (m *MockProxier) GetProxyProvider() proxy.Provider {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "GetProxyProvider")
-	ret0, _ := ret[0].(proxy.Provider)
-	return ret0
-}
-
-// GetProxyProvider indicates an expected call of GetProxyProvider.
-func (mr *MockProxierMockRecorder) GetProxyProvider() *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetProxyProvider", reflect.TypeOf((*MockProxier)(nil).GetProxyProvider))
 }
 
 // GetServiceByIP mocks base method.
@@ -99,4 +87,234 @@ func (m *MockProxier) GetServiceFlowKeys(serviceName, namespace string) ([]strin
 func (mr *MockProxierMockRecorder) GetServiceFlowKeys(serviceName, namespace any) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetServiceFlowKeys", reflect.TypeOf((*MockProxier)(nil).GetServiceFlowKeys), serviceName, namespace)
+}
+
+// OnEndpointSliceAdd mocks base method.
+func (m *MockProxier) OnEndpointSliceAdd(endpointSlice *v10.EndpointSlice) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnEndpointSliceAdd", endpointSlice)
+}
+
+// OnEndpointSliceAdd indicates an expected call of OnEndpointSliceAdd.
+func (mr *MockProxierMockRecorder) OnEndpointSliceAdd(endpointSlice any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnEndpointSliceAdd", reflect.TypeOf((*MockProxier)(nil).OnEndpointSliceAdd), endpointSlice)
+}
+
+// OnEndpointSliceDelete mocks base method.
+func (m *MockProxier) OnEndpointSliceDelete(endpointSlice *v10.EndpointSlice) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnEndpointSliceDelete", endpointSlice)
+}
+
+// OnEndpointSliceDelete indicates an expected call of OnEndpointSliceDelete.
+func (mr *MockProxierMockRecorder) OnEndpointSliceDelete(endpointSlice any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnEndpointSliceDelete", reflect.TypeOf((*MockProxier)(nil).OnEndpointSliceDelete), endpointSlice)
+}
+
+// OnEndpointSliceUpdate mocks base method.
+func (m *MockProxier) OnEndpointSliceUpdate(oldEndpointSlice, newEndpointSlice *v10.EndpointSlice) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnEndpointSliceUpdate", oldEndpointSlice, newEndpointSlice)
+}
+
+// OnEndpointSliceUpdate indicates an expected call of OnEndpointSliceUpdate.
+func (mr *MockProxierMockRecorder) OnEndpointSliceUpdate(oldEndpointSlice, newEndpointSlice any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnEndpointSliceUpdate", reflect.TypeOf((*MockProxier)(nil).OnEndpointSliceUpdate), oldEndpointSlice, newEndpointSlice)
+}
+
+// OnEndpointSlicesSynced mocks base method.
+func (m *MockProxier) OnEndpointSlicesSynced() {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnEndpointSlicesSynced")
+}
+
+// OnEndpointSlicesSynced indicates an expected call of OnEndpointSlicesSynced.
+func (mr *MockProxierMockRecorder) OnEndpointSlicesSynced() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnEndpointSlicesSynced", reflect.TypeOf((*MockProxier)(nil).OnEndpointSlicesSynced))
+}
+
+// OnEndpointsAdd mocks base method.
+func (m *MockProxier) OnEndpointsAdd(endpoints *v1.Endpoints) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnEndpointsAdd", endpoints)
+}
+
+// OnEndpointsAdd indicates an expected call of OnEndpointsAdd.
+func (mr *MockProxierMockRecorder) OnEndpointsAdd(endpoints any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnEndpointsAdd", reflect.TypeOf((*MockProxier)(nil).OnEndpointsAdd), endpoints)
+}
+
+// OnEndpointsDelete mocks base method.
+func (m *MockProxier) OnEndpointsDelete(endpoints *v1.Endpoints) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnEndpointsDelete", endpoints)
+}
+
+// OnEndpointsDelete indicates an expected call of OnEndpointsDelete.
+func (mr *MockProxierMockRecorder) OnEndpointsDelete(endpoints any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnEndpointsDelete", reflect.TypeOf((*MockProxier)(nil).OnEndpointsDelete), endpoints)
+}
+
+// OnEndpointsSynced mocks base method.
+func (m *MockProxier) OnEndpointsSynced() {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnEndpointsSynced")
+}
+
+// OnEndpointsSynced indicates an expected call of OnEndpointsSynced.
+func (mr *MockProxierMockRecorder) OnEndpointsSynced() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnEndpointsSynced", reflect.TypeOf((*MockProxier)(nil).OnEndpointsSynced))
+}
+
+// OnEndpointsUpdate mocks base method.
+func (m *MockProxier) OnEndpointsUpdate(oldEndpoints, endpoints *v1.Endpoints) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnEndpointsUpdate", oldEndpoints, endpoints)
+}
+
+// OnEndpointsUpdate indicates an expected call of OnEndpointsUpdate.
+func (mr *MockProxierMockRecorder) OnEndpointsUpdate(oldEndpoints, endpoints any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnEndpointsUpdate", reflect.TypeOf((*MockProxier)(nil).OnEndpointsUpdate), oldEndpoints, endpoints)
+}
+
+// OnNodeAdd mocks base method.
+func (m *MockProxier) OnNodeAdd(node *v1.Node) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnNodeAdd", node)
+}
+
+// OnNodeAdd indicates an expected call of OnNodeAdd.
+func (mr *MockProxierMockRecorder) OnNodeAdd(node any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnNodeAdd", reflect.TypeOf((*MockProxier)(nil).OnNodeAdd), node)
+}
+
+// OnNodeDelete mocks base method.
+func (m *MockProxier) OnNodeDelete(node *v1.Node) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnNodeDelete", node)
+}
+
+// OnNodeDelete indicates an expected call of OnNodeDelete.
+func (mr *MockProxierMockRecorder) OnNodeDelete(node any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnNodeDelete", reflect.TypeOf((*MockProxier)(nil).OnNodeDelete), node)
+}
+
+// OnNodeSynced mocks base method.
+func (m *MockProxier) OnNodeSynced() {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnNodeSynced")
+}
+
+// OnNodeSynced indicates an expected call of OnNodeSynced.
+func (mr *MockProxierMockRecorder) OnNodeSynced() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnNodeSynced", reflect.TypeOf((*MockProxier)(nil).OnNodeSynced))
+}
+
+// OnNodeUpdate mocks base method.
+func (m *MockProxier) OnNodeUpdate(oldNode, node *v1.Node) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnNodeUpdate", oldNode, node)
+}
+
+// OnNodeUpdate indicates an expected call of OnNodeUpdate.
+func (mr *MockProxierMockRecorder) OnNodeUpdate(oldNode, node any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnNodeUpdate", reflect.TypeOf((*MockProxier)(nil).OnNodeUpdate), oldNode, node)
+}
+
+// OnServiceAdd mocks base method.
+func (m *MockProxier) OnServiceAdd(service *v1.Service) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnServiceAdd", service)
+}
+
+// OnServiceAdd indicates an expected call of OnServiceAdd.
+func (mr *MockProxierMockRecorder) OnServiceAdd(service any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnServiceAdd", reflect.TypeOf((*MockProxier)(nil).OnServiceAdd), service)
+}
+
+// OnServiceDelete mocks base method.
+func (m *MockProxier) OnServiceDelete(service *v1.Service) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnServiceDelete", service)
+}
+
+// OnServiceDelete indicates an expected call of OnServiceDelete.
+func (mr *MockProxierMockRecorder) OnServiceDelete(service any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnServiceDelete", reflect.TypeOf((*MockProxier)(nil).OnServiceDelete), service)
+}
+
+// OnServiceSynced mocks base method.
+func (m *MockProxier) OnServiceSynced() {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnServiceSynced")
+}
+
+// OnServiceSynced indicates an expected call of OnServiceSynced.
+func (mr *MockProxierMockRecorder) OnServiceSynced() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnServiceSynced", reflect.TypeOf((*MockProxier)(nil).OnServiceSynced))
+}
+
+// OnServiceUpdate mocks base method.
+func (m *MockProxier) OnServiceUpdate(oldService, service *v1.Service) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "OnServiceUpdate", oldService, service)
+}
+
+// OnServiceUpdate indicates an expected call of OnServiceUpdate.
+func (mr *MockProxierMockRecorder) OnServiceUpdate(oldService, service any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OnServiceUpdate", reflect.TypeOf((*MockProxier)(nil).OnServiceUpdate), oldService, service)
+}
+
+// Run mocks base method.
+func (m *MockProxier) Run(stopCh <-chan struct{}) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "Run", stopCh)
+}
+
+// Run indicates an expected call of Run.
+func (mr *MockProxierMockRecorder) Run(stopCh any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Run", reflect.TypeOf((*MockProxier)(nil).Run), stopCh)
+}
+
+// SyncLoop mocks base method.
+func (m *MockProxier) SyncLoop() {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "SyncLoop")
+}
+
+// SyncLoop indicates an expected call of SyncLoop.
+func (mr *MockProxierMockRecorder) SyncLoop() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SyncLoop", reflect.TypeOf((*MockProxier)(nil).SyncLoop))
+}
+
+// SyncedOnce mocks base method.
+func (m *MockProxier) SyncedOnce() bool {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "SyncedOnce")
+	ret0, _ := ret[0].(bool)
+	return ret0
+}
+
+// SyncedOnce indicates an expected call of SyncedOnce.
+func (mr *MockProxierMockRecorder) SyncedOnce() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SyncedOnce", reflect.TypeOf((*MockProxier)(nil).SyncedOnce))
 }
